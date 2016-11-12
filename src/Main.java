b4b852035f852e11f8889722d3e690784dc7708a
Burak <info@burakkutbay.com>
2016-11-12 22:51:16 +0300

    Topla Metodu ve Javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 /**
  * Bu bir JavaDoc Örneğidir.
+ *
  * @author burakkutbay
  * @version 1.0.0
- * @since  release
+ * @since  2016-11-12
  */
 public class Main {
+
+    /**
+     * topla metodu iki sayıyı toplar ve sonucu döndürür
+     * @param sayi1
+     * @param sayi2
+     * @return
+     */
+    public int topla(int sayi1, int sayi2){
+        int sonuc=sayi1+sayi2
+        return sonuc;
+    }
 
     public static void main(String[] args) {
         System.out.println("Javadoc Example by Burak KUTBAY");
